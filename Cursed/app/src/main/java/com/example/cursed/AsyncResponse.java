1a1480c89f5855e9cf47fcce53eb265e2a3060f2
84beiroun <65925569+84beiroun@users.noreply.github.com>
2022-05-02 14:13:47 +0600

    Delete Cursed directory

--- Cursed/app/src/main/java/com/example/cursed/AsyncResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.cursed;
-
-public interface AsyncResponse {
-    void processFinish(String output);
-    String fullUrl = null;
-}
--- Cursed/app/src/main/java/com/example/cursed/AsyncValidate.java
+++
@@ -1,6 +0,0 @@
-package com.example.cursed;
-
-public interface AsyncValidate {
-    void processFinish(boolean output);
-    boolean validate = false;
-}
